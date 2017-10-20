f52762d672bdd6ed12ff1370839afefcb12d21dd
Tobias Scheck <tobias@scheck-media.de>
2017-10-20 14:40:59 +0200

    removed model_plot from test.py

--- test.py
+++ test.py
@@ -1,5 +1,3 @@
 from shufflenet import ShuffleNet
-from keras.utils import plot_model
 
 model = ShuffleNet()
-plot_model(model, to_file='/var/www/model_groups_%d.svg' % 1, show_shapes=True, show_layer_names=True)
