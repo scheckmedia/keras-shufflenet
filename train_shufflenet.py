5c701cba652ec96820293b0fd9117d26272f20c2
Tobias Scheck <tobias@scheck-media.de>
2017-11-11 22:02:19 +0100

    implementation learn rate deacy every 30 epochs, changed optimizer

--- train_shufflenet.py
+++ train_shufflenet.py
@@ -1,10 +1,15 @@
 import keras
 from keras.preprocessing.image import ImageDataGenerator
+from keras.applications.imagenet_utils import preprocess_input
+from keras.utils import plot_model
 from shufflenet import ShuffleNet
-from keras.callbacks import CSVLogger, ModelCheckpoint, ReduceLROnPlateau
+from keras.callbacks import CSVLogger, ModelCheckpoint, ReduceLROnPlateau, LearningRateScheduler
+import numpy as np
 
 
 def preprocess(x):
+    x = np.expand_dims(x, axis=0)
+    x = preprocess_input(x)
     x /= 255.0
     x -= 0.5
     x *= 2.0
@@ -14,17 +19,23 @@ def preprocess(x):
 if __name__ == '__main__':
     groups = 3
     batch_size = 128
+    inital_epoch = 0
     ds = '/mnt/daten/Development/ILSVRC2012_256'
 
-    model = ShuffleNet(groups=groups, pooling='max')
-    model.load_weights('shufflenet_g3.hdf5', by_name=True)
-    csv_logger = CSVLogger('%s.log' % model.name)
-    checkpoint = ModelCheckpoint(filepath='%s.hdf5' % model.name, verbose=1, save_best_only=True,
-                                   monitor='val_acc', mode='max')
+    model = ShuffleNet(groups=groups, pooling='avg')
+    plot_model(model, 'model.png', show_shapes=True)
+    # model.load_weights('%s.hdf5' % model.name, by_name=True)
+    csv_logger = CSVLogger('%s.log' % model.name, append=(inital_epoch is not 0))
+    checkpoint = ModelCheckpoint(filepath='%s.hdf5' % model.name, verbose=1,
+                                 save_best_only=True, monitor='val_acc', mode='max')
+
+    learn_rates = [0.05, 0.01, 0.005, 0.001, 0.0005]
+    lr_scheduler = LearningRateScheduler(lambda epoch: learn_rates[epoch // 30])
 
     train_datagen = ImageDataGenerator(preprocessing_function=preprocess,
-                                       shear_range=0.05,
-                                       zoom_range=0.05,
+                                       zoom_range=0.25,
+                                       width_shift_range=0.05,
+                                       height_shift_range=0.05,
                                        horizontal_flip=True)
 
     test_datagen = ImageDataGenerator(preprocessing_function=preprocess)
@@ -40,18 +51,17 @@ if __name__ == '__main__':
             batch_size=batch_size)
 
     model.compile(
-              optimizer=keras.optimizers.Adam(decay=1e-5),
+              optimizer=keras.optimizers.SGD(lr=.05, decay=5e-4, momentum=0.9),
               metrics=['accuracy'],
               loss='categorical_crossentropy')
-
-    reduce_lr = ReduceLROnPlateau(monitor='val_loss', patience=4, cooldown=0)
 
     model.fit_generator(
             train_generator,
             steps_per_epoch=train_generator.samples // batch_size,
-            epochs=50,
-            workers=6,
+            epochs=100,
+            workers=7,
+            initial_epoch=inital_epoch,
             use_multiprocessing=False,
             validation_data=test_generator,
             validation_steps=test_generator.samples // batch_size,
-            callbacks=[csv_logger, checkpoint, reduce_lr])
+            callbacks=[csv_logger, checkpoint, lr_scheduler])
